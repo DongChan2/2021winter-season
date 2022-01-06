32a211cb5d224b61c122661911efb727f14bb2a7
DongChan2 <dc8933@naver.com>
2022-01-06 14:08:49 +0900

    second commit

--- Term-Project_m.py
+++ Term-Project_m.py
@@ -3,6 +3,23 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 
+def tridiag(a1,b1,c1,c,rhs):
+    rhs[1]=rhs[1]-a1[1]*c[0]
+    rhs[m-1]=rhs[m-1]-c1[m-1]*c[m]    
+
+    #Thomas Algorithm
+    for i in range(2,m):
+        r=a1[i]/b1[i-1]
+        b1[i]=b1[i]-r*c1[i-1]
+        rhs[i]=rhs[i]-r*rhs[i-1]
+
+    rhs[m-1]=rhs[m-1]/b1[m-1]
+    for i in range(m-2,0,-1):
+        rhs[i]=(rhs[i]-c1[i]*rhs[i+1])/b1[i]
+        
+    return rhs        
+    
+
 #Definitions and initializations of variable arrays
 
 m=100
@@ -54,28 +71,27 @@ f1=((2-alpha*pe)*v*(1-theta))/(2*pe*dx)
 #Time integral(Time marching)
 for iter in range(1,nmax+1):
     if iter==round(iter/100)*100:
-        print("{0}".format(iter))
-    for i in range(1,m):
-        a1[i]=float((pe*alpha-2*pe-2)*v*theta/(2*pe*dx))
-        b1[i]=float((1/dt)+((1-alpha)*v*theta)/dx+(2*v*theta)/(pe*dx))
-        c1[i]=float(((alpha*pe-2)*v*theta)/(2*pe*dx))  
-        rhs[i]=d1*c[i-1]+e1*c[i]+f1*c[i+1]
-    rhs[1]=rhs[1]-a1[1]*c[0]
-    rhs[m-1]=rhs[m-1]-c1[i]*c[m]    
-
-    #Thomas Algorithm
-    for i in range(2,m):
-        r=a1[i]/b1[i-1]
-        b1[i]=b1[i]-r*c1[i-1]
-        rhs[i]=rhs[i]-r*rhs[i-1]
-
-    rhs[m-1]=rhs[m-1]/b1[m-1]
-    for i in range(m-2,0,-1):
-        rhs[i]=(rhs[i]-c1[i]*rhs[i+1])/b1[i]
-        
-    #renewal of concentration at the next time step
-    for i in range(1,m):
-        c[i]=rhs[i]
+        print("{0}".format(iter))    
+   
+    if scheme==1:
+        for i in range(1,m):
+            a1[i]=float((pe*alpha-2*pe-2)*v*theta/(2*pe*dx))
+            b1[i]=float((1/dt)+((1-alpha)*v*theta)/dx+(2*v*theta)/(pe*dx))
+            c1[i]=float(((alpha*pe-2)*v*theta)/(2*pe*dx))  
+            rhs[i]=d1*c[i-1]+e1*c[i]+f1*c[i+1]
+            
+        rhs=tridiag(a1,b1,c1,c,rhs)
+            
+            #renewal of concentration at the next time step
+        for i in range(1,m):
+            c[i]=rhs[i]
+        #explicit method   
+    elif scheme==2:
+        for i in range(1,m):
+            b1[i]=float((1/dt)+((1-alpha)*v*theta)/dx+(2*v*theta)/(pe*dx))
+            rhs[i]=d1*c[i-1]+e1*c[i]+f1*c[i+1]
+            c[i]=rhs[i]/b1[i]
+                    
     #renewal of boundary conditions
     c[0]=1.0
     c[m]=c[m-1]
@@ -86,6 +102,7 @@ for i in range(0,m+1):
     y2=(x[i]+v*dt*nmax)/(2*math.sqrt(d*dt*nmax))
     ce[i]=c[0]/2*(math.erfc(y1)+math.exp(v*x[i]/d)*math.erfc(y2))    
 
+ 
 #Plot of numerical solution
 #print plots
 plt.figure(figsize=(30,10),dpi=60)
@@ -114,3 +131,5 @@ plt.axis([0,100,0,1])
 plt.grid(True)
 plt.savefig("result.jpg")
 plt.show()
+
+ 
--- abcd.py
+++
@@ -1,6 +0,0 @@
-from mypackage import calculator
-
-print(calculator.plus(10,5))
-print(calculator.minus(10,5))
-print(calculator.multiply(10,5))
-print(calculator.divide(10,5))
